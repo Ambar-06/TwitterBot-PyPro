e20505f3fe7da959958f590aeda4f2d0d5bc4634
Ambar Rizvi <110850152+Ambar-06@users.noreply.github.com>
2022-12-15 19:21:09 +0530

    Update tb.py

--- tb.py
+++ tb.py
@@ -25,11 +25,6 @@ with open("details.csv") as file:
             print(e)
             print("Authentication Error")
 
-                                    # Reading TimeLine
-                                    # time_line = api.home_timeline()
-                                    # for the_tweet in time_line:  
-                                    #     print(f"{the_tweet.user.name} said {the_tweet.text}") 
-
                                     # api.update_status("This is a test tweet to learn Tweepy Python") 
         quote_raw = requests.get('https://api.quotable.io/random')
         quote = quote_raw.json()
